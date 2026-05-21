52bede84436bc24739434e53d94d91f030409870
Kozyrin Semen <garbagemilitia@gmail.com>
2026-05-21 18:48:22 +0300

    Removed base and downgraded the model to a mixin

--- src/simpleauth/model.py
+++ src/simpleauth/model.py
@@ -2,13 +2,9 @@ from uuid import uuid4, UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class Base(DeclarativeBase):
-    pass
 
 
-class UserBaseModel(Base):
-    __tablename__ = "users"
-
+class UserMixin(Base):
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
