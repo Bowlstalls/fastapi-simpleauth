fc601e4b3c0a36a22423f956071244bb63f4c6a5
Kozyrin Semen <garbagemilitia@gmail.com>
2026-05-21 18:49:13 +0300

    Update __init__

--- src/simpleauth/__init__.py
+++ src/simpleauth/__init__.py
@@ -1,3 +1,9 @@
-from .model import UserBaseModel
+from .model import UserMixin
 from .router import get_auth_router
 from .simpleauth import SimpleAuth
+
+__all__ = [
+    "UserMixin",
+    "get_auth_router",
+    "SimpleAuth"
+]
