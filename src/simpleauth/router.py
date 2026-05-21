cd2bd9f75a71b795c825ce341b1c735772e13747
Kozyrin Semen <garbagemilitia@gmail.com>
2026-05-21 17:47:37 +0300

    Add router

---
+++ src/simpleauth/router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .simpleauth import SimpleAuth
+from .model import UserBaseModel
+from .schemas import UserCreateBase, UserReadBase
+
+
+def get_auth_router(auth: SimpleAuth, read: type[UserReadBase], create: type[UserCreateBase]):
+    router = APIRouter(prefix="/auth", tags=["auth"])
+
+    @router.post("/register", response_model=read)
+    async def register(data: create, session: AsyncSession = Depends(auth.get_session)):
+        return await auth._create_user(data.name, data.password, session)
+
+    @router.post("/login")
+    async def login(data: create, session: AsyncSession = Depends(auth.get_session)):
+        return {
+            "token": await auth._create_token(data.name, data.password, session),
+            "token_type": "Bearer"
+        }
+
+    @router.get("/me", response_model=read)
+    async def me(user: type[UserBaseModel] = Depends(auth.get_current_user())):
+        return user
+
+    return router
