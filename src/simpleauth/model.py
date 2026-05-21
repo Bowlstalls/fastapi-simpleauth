ed66b896372b119739bc9b61018665061ee355d6
Kozyrin Semen <garbagemilitia@gmail.com>
2026-05-21 18:59:32 +0300

    Remove Base from UserMixin

--- src/simpleauth/model.py
+++ src/simpleauth/model.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 
-class UserMixin(Base):
+class UserMixin:
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
