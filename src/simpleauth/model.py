282b9057abc05e273e6afd226204ed45773856ce
Kozyrin Semen <garbagemilitia@gmail.com>
2026-05-21 17:46:20 +0300

    Add base user model

---
+++ src/simpleauth/model.py
@@ -0,0 +1,14 @@
+from uuid import uuid4, UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class UserBaseModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[bytes] = mapped_column()
