d8d7e1cf6b8b7da38cfc6256732118f4d209433c
KozyrinSemen <garbagemilitia@gmail.com>
2026-06-04 19:46:47 +0900

    Add test main file

---
+++ tests/main.py
@@ -0,0 +1,30 @@
+from typing import AsyncGenerator, Any
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from src.simpleauth.model import UserMixin
+from src.simpleauth.router import get_auth_router
+from src.simpleauth.schemas import UserReadBase, UserCreateBase
+from src.simpleauth.simpleauth import SimpleAuth
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(UserMixin, Base):
+    __tablename__ = "users"
+
+
+engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+session_maker = async_sessionmaker(engine)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, Any]:
+    async with session_maker() as session:
+        yield session
+
+
+auth = SimpleAuth[User]("secret", User, get_session)
+app = FastAPI()
+app.include_router(get_auth_router(auth, UserReadBase, UserCreateBase))
