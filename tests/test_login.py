024181332db5e8da0ade1c438603e1c01c4acea3
KozyrinSemen <garbagemilitia@gmail.com>
2026-06-05 02:20:45 +0900

    Add login test

---
+++ tests/test_login.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_login(client, registered_user):
+    response = await client.post(
+        "auth/login",
+        json=registered_user
+    )
+    print(response.json())
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_invalid_credentials(client):
+    response = await client.post(
+        "auth/login",
+        json={
+            "username": "wrong_username",
+            "password": "wrong_password"
+        }
+    )
+    assert response.status_code == 401
+    assert response.json()["detail"] == "invalid credentials"
