ed7b6b01e0b5414dcaa89611d57cf2777215c8dc
KozyrinSemen <garbagemilitia@gmail.com>
2026-06-05 20:22:06 +0900

    Fix way too many blank lines

--- src/fastapi-simpleauth/model.py
+++ src/fastapi-simpleauth/model.py
@@ -2,8 +2,6 @@ from uuid import uuid4, UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-
-
 class UserMixin:
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
